3690fec2ee89df577f676f93e2b136d69cc082e7
GulDilin <zhenyagurin@gmail.com>
2021-03-26 01:48:38 +0300

    Add findByTitleMethod

--- src/main/java/guldilin/repository/WorkerRepository.java
+++ src/main/java/guldilin/repository/WorkerRepository.java
@@ -1,7 +1,16 @@
 package guldilin.repository;
 
 import guldilin.model.Worker;
+import guldilin.model.WorkerRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
+    List<Worker> findWorkerByRoleLevel(Integer level);
+
+    List<Worker> findWorkerByRoleTitle(String title);
+
+    List<Worker> findWorkerByRole(WorkerRole role);
 }
