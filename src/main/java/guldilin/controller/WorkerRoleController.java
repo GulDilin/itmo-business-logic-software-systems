c9f777bf0914686b5a0063a94d409220cb3ceb90
GulDilin <zhenyagurin@gmail.com>
2021-03-26 04:43:05 +0300

    add bad requests handling

--- src/main/java/guldilin/controller/WorkerRoleController.java
+++ src/main/java/guldilin/controller/WorkerRoleController.java
@@ -3,8 +3,11 @@ package guldilin.controller;
 import guldilin.dto.WorkerRoleDTO;
 import guldilin.service.WorkerRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -48,5 +51,18 @@ public class WorkerRoleController {
             }};
             return ResponseEntity.badRequest().body(errorMap);
         }
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
