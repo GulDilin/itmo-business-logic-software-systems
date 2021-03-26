ec03681748b5e630ad2561bbe066054b36699960
GulDilin <zhenyagurin@gmail.com>
2021-03-26 03:28:41 +0300

    Remove useless import

--- src/main/java/guldilin/repository/WorkerRoleRepository.java
+++ src/main/java/guldilin/repository/WorkerRoleRepository.java
@@ -1,10 +1,8 @@
 package guldilin.repository;
 
-import guldilin.model.WorkerRole;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
-import java.util.Optional;
+import guldilin.model.WorkerRole;
 
 public interface WorkerRoleRepository extends JpaRepository<WorkerRole, Long> {
     List<WorkerRole> findAllByTitle(String title);
