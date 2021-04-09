4e7be740cf19e8dc69365ade3a365c6d34a815b5
GulDilin <zhenyagurin@gmail.com>
2021-04-09 03:17:56 +0300

    remove unused imports

--- src/main/java/guldilin/repository/ProducerRepository.java
+++ src/main/java/guldilin/repository/ProducerRepository.java
@@ -4,7 +4,6 @@ import guldilin.model.Producer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ProducerRepository extends JpaRepository<Producer, Long> {
     List<Producer> findAllByTitle(String title);
