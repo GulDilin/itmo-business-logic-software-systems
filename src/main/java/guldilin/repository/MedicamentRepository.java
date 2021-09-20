8962fd74a4db3a964f800c1349a8ec405c1a5c13
GulDilin <zhenyagurin@gmail.com>
2021-09-20 13:50:04 +0300

    remove unused methods

--- src/main/java/guldilin/repository/MedicamentRepository.java
+++ src/main/java/guldilin/repository/MedicamentRepository.java
@@ -7,10 +7,7 @@ import java.util.List;
 
 public interface MedicamentRepository extends JpaRepository<Medicament, Long> {
     List<Medicament> findAllByTitle(String title);
-    List<Medicament> findAllByActiveSubstances(ActiveSubstance activeSubstance);
     List<Medicament> findAllByActiveSubstancesId(Long activeSubstanceId);
-    List<Medicament> findAllByFormula (MedicamentFormula formula);
     List<Medicament> findAllByFormulaId (Long formulaId);
-    List<Medicament> findAllByGroup (MedicamentGroup group);
     List<Medicament> findAllByGroupId (Long groupId);
 }
